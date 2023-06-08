fafa4e57b0a8317954e5092a6607d41106602449
minaMerzeq <minamerzekluffy@gmail.com>
2023-06-08 10:53:00 +0300

    application contracts: IGenericRepository and interfaces

---
+++ HR.LeaveManagment.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿// contracts states what you want to do by using interfaces not how you want to do it which is the implementation
+// persistence refers to data access
+
+namespace HR.LeaveManagment.Application.Contracts.Persistence;
+
+// there r many opinions on using IGenericRepository or not since EF core dataContext operates as repository / UnitOfWork
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
---
+++ HR.LeaveManagment.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -0,0 +1,8 @@
+﻿using HR.LeaveManagment.Domain;
+
+namespace HR.LeaveManagment.Application.Contracts.Persistence;
+
+public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+{
+
+}
